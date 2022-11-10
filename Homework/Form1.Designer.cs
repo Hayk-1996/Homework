c94a73a0eb74429ff3a27796d1afd0bf64a755cf
Hayk-1996 <hayk98837073@gmail.com>
2022-11-10 05:05:13 -0800

    Add project files.

---
+++ Homework/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Homework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pacMen = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pacMen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pacMen
+            // 
+            this.pacMen.BackColor = System.Drawing.Color.Red;
+            this.pacMen.Location = new System.Drawing.Point(0, 0);
+            this.pacMen.Name = "pacMen";
+            this.pacMen.Size = new System.Drawing.Size(63, 58);
+            this.pacMen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pacMen.TabIndex = 0;
+            this.pacMen.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.mainGame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(734, 361);
+            this.Controls.Add(this.pacMen);
+            this.MaximumSize = new System.Drawing.Size(750, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pacMen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pacMen;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Homework/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Homework
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft, goRight, goUp, goDown;
+        int speed = 7;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void mainGame(object sender, EventArgs e)
+        {
+            if (goLeft == true && pacMen.Left > 0)
+            {
+                pacMen.Left -= speed;
+            }
+
+            if (goRight == true && pacMen.Right < 735)
+            {
+                pacMen.Left += speed;
+            }
+
+            if (goUp == true && pacMen.Top > 0)
+            {
+                pacMen.Top -= speed;
+            }
+            if (goDown == true && pacMen.Bottom < 355)
+            {
+                pacMen.Top += speed;
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = true;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+        }
+    }
+}
